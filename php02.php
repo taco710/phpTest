dd982f2bcfed3e20362866669c2700d88a2a46a0
taco710 <kota.yamashita@rakuten-sec.co.jp>
2021-08-25 09:53:12 +0900

    課題

---
+++ php02.php
@@ -0,0 +1,29 @@
+<?php
+$name = 'kota';
+if ($name = 'kta') {
+    echo "私はkotaです";
+}
+  else {"kotaではありません";
+}
+echo "\n";
+$total = 0;
+for($i = 0; $i <= 10000; $i++){
+    $total += $i;
+}
+echo $total;
+echo "\n";
+$fruits = array("apple", "orange", "lemon", "grape", "melon");
+foreach($fruits as $fruits){
+    echo " " . $fruits;
+    echo "\n";
+  }
+  echo "\n";
+  $start = 1;
+  $end = 100;
+
+for($i = $start; $i < $end; $i++){
+    if($i % 5 == 0){
+        echo $i;
+        echo "\n"; }
+}
+?>
